0802609a291efd1e33c69788e0135b996f82ff7b
ethanchang13 <526ychang@frhsd.com>
2024-10-22 11:53:45 -0400

    fixed timeline

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-var timelineSwiper = new Swiper('.timeline .swiper-container', {
-    direction: 'vertical',
-    loop: false,
-    speed: 1600,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-            var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-            return '<span class="' + className + '">' + year + '</span>';
-        }
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-        768: {
-            direction: 'horizontal',
-        }
-    }
+$(function(){
+    $().timeline({
+        orientation: 	'vertical',
+        issuesSpeed: 	300,
+        datesSpeed: 	100,
+        arrowKeys: 		'true',
+        startAt:		3
+    })
 });
